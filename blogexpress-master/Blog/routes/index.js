4202c3a8920705192bc7b44a291027e98537123c
yangfan789789 <1062208122@qq.com>
2019-11-09 19:34:30 +0800

    yangfan

---
+++ blogexpress-master/Blog/routes/index.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+var path =require("path");
+var fs = require("fs");
+
+
+let filepath = path.join(__dirname,'data.json');
+let filecontent = fs.readFileSync(filepath);
+let jsonobj = JSON.parse(filecontent);
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+router.post('/login', function(req, res, next) {
+  // res.end()
+
+  let username = req.body.username;
+  let pwd = req.body.pwd;
+  console.log(jsonobj);
+  for(var i=0;i<jsonobj.users.length;i++){
+    if(jsonobj.users[i].username === username && jsonobj.users[i].password === pwd){
+      res.end("success");
+    }else{
+      res.end("failed");
+    }
+  }
+
+
+ 
+});
+
+router.get('/list', function(req, res, next) {
+  res.render('list',{chapterList:jsonobj.chapterList});
+});
+
+module.exports = router;
